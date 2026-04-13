77371cd3c4b8d6e40dde33acfdc19079108332b3
Pranav <pranavnaik543@gmail.com>
2026-04-13 11:28:58 -0400

    Fix CORS allowing Netlify origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Zealthy EMR & Patient Portal API")
 from fastapi.middleware.cors import CORSMiddleware 
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]) 
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000", "https://mini-emr-application.netlify.app"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]) 
 
 
 # Dependency
